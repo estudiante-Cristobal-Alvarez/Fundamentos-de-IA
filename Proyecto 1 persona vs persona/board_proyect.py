d1a1e9c8e3fb7ec48e7d77de6c69a435630d3d52
Cristobal Alvarez <cristobal.alvarezcataldo@gmail.com>
2026-08-23 21:33:54 -0400

    Cambios en board

---
+++ Proyecto 1 persona vs persona/board_proyect.py
@@ -0,0 +1,105 @@
+class Board:
+    """Clase base para representar un tablero cuadrado de tamaño n x n."""
+
+    __places: list[list[str]]  # Tablero en sí
+    __size: int  # Tamaño del tablero
+
+    EMPTY_SPACE = "."  # Constante de clase que marca los espacios vacíos
+        
+        def __init__(self, n: int = ):
+            """Crea un tablero cuadrado de tamaño n x n"""
+            if n <= 0:
+                raise ValueError("Debe seleccionar un número mayor a 0 para el tablero")
+            
+            # Define la lista para almacenar las posiciones
+            self.__places = [
+                [Board.EMPTY_SPACE] * n for _ in range(n)                            
+            ]
+            self.__size = n
+    
+        def __str__(self) -> str:
+            """Retorna una representación legible del tablero)"""
+            
+            # Cantidad de caracteres para la columna con número de fila
+            offset = math.ceil(math.log10(self.__size))
+            # Primera línea
+            board = " "*offset + " "
+            board += " ".join(chr(ord('A') + i) for i in range(self.__size)) + "\n"
+            for i, line in enumerate(self.__places, 1):
+                # Falta arreglar el ancho del primer número
+                board += f"{i} " + " ".join(line) + '\n'
+            return board
+    
+        def __repr__(self) -> str:
+            """Función para cuando se llama repr(self)"""
+            return f"Board({self.__size})"
+    
+        def __len__(self) -> int:
+            """Función para cuando se llama len(self)"""
+            return self.__size
+    
+        def __check_valid_range(self, r: int) -> bool:
+            """Valida que el valor esté dentro del rango del tablero
+            
+            Esto considera que las posiciones van de 1 a n
+            """
+            # Nombre con dos guiones bajos al inicio se interpreta como privada
+            # En relidad, Python le cambia internamente el nombre, porque no tiene
+            # el concepto real de atributo o método privado
+            if 1 > r or r > self.__size:
+                return False
+            return True
+    
+        def __getitem__(self, subscript: int | tuple):
+            """Implementa self[subscript]
+            
+            En este caso, `subscript` puede ser un entero (fila) o una tupla
+            (coordenadas).
+            
+            Levanta excepciones, si no se usa bien.
+            """
+            if isinstance(subscript, tuple):
+                # Si es una tupla
+                # Si son más o menos que filas y columnas
+                if len(subscript) != 2:
+                    raise ValueError("Cooordinates with too many dimensions")
+                # Si la fila está fuera de rangoo
+                if not self.__check_valid_range(subscript[0]):
+                    raise LookupError(f"Row out of range: {subscript[0]}")
+                # Si la columna está fuera de rango
+                if not  self.__check_valid_range(subscript[1]):
+                    raise LookupError(f"Column out of range: {subscript[1]}")
+                return self.__places[subscript[0] - 1][subscript[1] - 1]
+            elif isinstance(subscript, int):
+                # Si es un entero
+                if not self.__check_valid_range(subscript):
+                    raise LookupError(f"Row out of range: {subscript}")
+                return self.__places[subscript - 1]
+            else:
+                # Si el índice no es del tipo correcto
+                raise TypeError("Subscript must be integer or coordinates")
+        
+        def __setitem__(self, key: tuple, value: str) ->  None:
+            """Implementa self[key] = value
+            
+            El "índice" `key` tiene que ser un par de coordenadas
+            """
+            if not isinstance(key, tuple):
+                raise TypeError(f"Subscript must be coordinates (tuple), not {type(key)}")
+            if len(key) != 2:
+                raise ValueError("Cooordinates with too many dimensions")
+            # Si la fila está fuera de rangoo
+            if not self.__check_valid_range(key[0]):
+                raise LookupError(f"Row out of range: {key[0]}")
+            # Si la columna está fuera de rango
+            if not  self.__check_valid_range(key[1]):
+                raise LookupError(f"Column out of range: {key[1]}")
+            self.__places[key[0] - 1][key[1] - 1] = value
+    
+        def valid_move(self, r: int, c: int):
+            """Valida que sea un movimiento válido, es decir, a una casilla libre
+            
+            Este método debería ser sobrecargado por un tablero hijo que
+            permite movimientos válidos con otras reglas
+            """
+            return self[r, c] == Board.EMPTY_SPACE
